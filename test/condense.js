ad3774008612665ccbb81ed06976f0b1e928cf89
Quinn Slack <qslack@qslack.com>
2014-01-10 02:52:40 -0800

    require_empty_deps

--- test/condense.js
+++ test/condense.js
@@ -78,4 +78,5 @@ exports.runTests = function(filter) {
 
   test({load: ["require_const"], plugins: {requirejs: true}});
   test({load: ["require_setup"], plugins: {requirejs: true}});
+  test({load: ["require_empty_deps"], plugins: {requirejs: true}});
 };
