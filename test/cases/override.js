eee1acd5af1747aa75932824a703079edd3f94da
Quinn Slack <qslack@qslack.com>
2014-01-17 22:19:23 -0800

    AST override plugin for overriding scope entries based on AST matches

---
+++ test/cases/override.js
@@ -0,0 +1,20 @@
+// plugin=override [{"node":{"type":"Identifier","name":"A"},"def":{"A":"string"}},{"node":{"type":"MemberExpression","object":{"type":"Identifier","name":"B"},"property":"c"},"def":{"B":{"c":"number"}}},{"node":{"type":"MemberExpression","object":{"type":"Identifier","name":"D"},"property":"e"},"def":{"D":{"e":{"!type":"fn(obj: ?) -> !0"}}}}]
+
+var A = 7;
+A; //: string
+
+var B = {c: "foo"};
+B.c; //: number
+
+var D = {e: function() {}};
+D.e(1); //: number
+D.e(true); //: bool
+
+function() {
+  var D = {e: "asdf"};
+  D.e(function(){}); //: fn()
+}
+
+D = 7;
+D.e = 5;
+D.e("a"); //: string
